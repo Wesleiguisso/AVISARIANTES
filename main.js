ccbb89782251a3c1cc58b9c5c0e95b9d77328aa9
Wesleiguisso <124799703+Wesleiguisso@users.noreply.github.com>
2024-04-26 13:52:10 -0300

    .

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-04-17T23:59:00");
-const tempoObjetivo2 = new Date("2024-06-17T00:40:00");
-const tempoObjetivo3 = new Date("2024-8-30T00:20:00");
+const tempoObjetivo2 = new Date("2024-08-30T00:40:00");
+const tempoObjetivo3 = new Date("2024-06-17T00:20:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:15:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
